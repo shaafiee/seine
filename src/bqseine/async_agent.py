6e3ea0473d90000717e69de9a58fac8cd639a102
Shaafiee <mohamed.shaafiee@comfort-works.com>
2026-05-07 17:01:24 +0000

    refining job resolution

--- src/bqseine/async_agent.py
+++ src/bqseine/async_agent.py
@@ -495,7 +495,7 @@ def process(job = '',
 			thinking_config=gtypes.ThinkingConfig(thinking_budget=thinking) 
 		)
 
-	resp = completed_job.results[0].response
+	resp = completed_job.inlined_responses[0].response
 
 	# Collect any tool calls from parts
 	tool_calls = []
